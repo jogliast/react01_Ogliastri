fc6e084b68011d249c96d8383471f78b93e48d88
jogliast <jogliast@cuc.edu.co>
2024-09-18 09:57:05 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import miImagen from "./assets/img/logoReact.png";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={miImagen} alt="logo react" />
+        <h1>hola mundo!!</h1>
+        <p>este es mi primer proyecto en react..</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
